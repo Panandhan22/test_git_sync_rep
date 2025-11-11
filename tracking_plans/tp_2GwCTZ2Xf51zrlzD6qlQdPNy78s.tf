48c88fad250f4f91d386a8921b22eeaacb85b9cf
Segment Git Sync <friends@segment.com>
2025-11-11 06:44:32 +0000

    Full sync

---
+++ tracking_plans/tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s.tf
@@ -0,0 +1,12 @@
+import {
+  to = segment_tracking_plan.id-tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s
+  id = "tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s"
+}
+
+resource "segment_tracking_plan" "id-tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s" {
+  description = "description"
+  name        = "Untitled Tracking Plan"
+  rules = [
+  ]
+  type = "LIVE"
+}
