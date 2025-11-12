de5e0ef365895a741a43ac806d9288196fb577e3
Segment Git Sync <friends@segment.com>
2025-11-12 06:58:53 +0000

    Source '7GKdv96gwNcNMMH4Zaivfx' modified by internal-service@segment.com

---
+++ sources/7GKdv96gwNcNMMH4Zaivfx.tf
@@ -0,0 +1,15 @@
+import {
+  to = segment_source.id-7GKdv96gwNcNMMH4Zaivfx
+  id = "7GKdv96gwNcNMMH4Zaivfx"
+}
+
+resource "segment_source" "id-7GKdv96gwNcNMMH4Zaivfx" {
+  enabled = true
+  labels  = null
+  metadata = {
+    id = "yBvi77aEwr"
+  }
+  name     = "Go"
+  settings = jsonencode({})
+  slug     = "go"
+}
