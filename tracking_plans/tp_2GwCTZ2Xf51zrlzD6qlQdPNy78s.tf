5cdf79a6f6882e60745325d6d90e61b4c16d7f72
Segment Git Sync <friends@segment.com>
2025-11-12 05:52:23 +0000

    Tracking Plan 'tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s' modified by priya.anandhan@segment.com

--- tracking_plans/tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s.tf
+++ tracking_plans/tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s.tf
@@ -7,6 +7,24 @@ resource "segment_tracking_plan" "id-tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s" {
   description = "description"
   name        = "Untitled Tracking Plan"
   rules = [
+    {
+      json_schema = jsonencode({
+        "$schema" = "http://json-schema.org/draft-07/schema#"
+        properties = {
+          context = {}
+          properties = {
+            type = "object"
+          }
+          traits = {
+            type = "object"
+          }
+        }
+        type = "object"
+      })
+      key     = "Order Completed"
+      type    = "TRACK"
+      version = 1
+    },
   ]
   type = "LIVE"
 }
