aaf03c43a66a818dfd9200ee89c57e5fd2803d21
Panandhan22 <115441424+Panandhan22@users.noreply.github.com>
2025-11-13 09:00:47 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ provider "random" {}
 
 # Example resource
 resource "segment_source_tracking_plan_connection" "example" {
-  source_id        = "7GKdv96gwNcNMMH4Zaivfx"
-  tracking_plan_id = "tp_2GwCTZ2Xf51zrlzD6qlQdPNy78s"
+  source_id        = ""
+  tracking_plan_id = ""
 }
